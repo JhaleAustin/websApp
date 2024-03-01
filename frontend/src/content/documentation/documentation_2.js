e8d7ddf74e970eea9d9b9c34139a1ee229c0896e
JhaleAustin <atheahisuan36@gmail.com>
2024-03-01 21:37:28 +0800

    updated

--- frontend/src/content/documentation/documentation_2.js
+++ frontend/src/content/documentation/documentation_2.js
@@ -42,11 +42,11 @@ function Documentation_2() {
 
         <div className="mx-auto max-w-7xl px-6 lg:px-8"  style={{ background: 'rgba(0, 0, 0, 0.5)', width: 800, margin: '50px auto',padding:50, borderRadius: 10 }}>
           <div className="mx-auto max-w-2xl lg:text-center">
-            <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
-            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <h2 className="text-base font-semibold leading-7" style={{color: '#ddcdbc'}}>Deploy faster</h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl" style={{color: '#f8ddc2'}}>
               Everything you need to deploy your app
             </p>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <p className="mt-6 text-lg leading-8 text-gray-600" style={{color: '#f4e4cd'}}>
               Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
               pulvinar et feugiat blandit at. In mi viverra elit nunc.
             </p>
@@ -55,13 +55,13 @@ function Documentation_2() {
             <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
               {features.map((feature) => (
                 <div key={feature.name} className="relative pl-16">
-                  <dt className="text-base font-semibold leading-7 text-gray-900">
+                  <dt className="text-base font-semibold leading-7 text-gray-900" style={{color: '#e8b8a5'}}>
                     <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                       <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
                     </div>
                     {feature.name}
                   </dt>
-                  <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+                  <dd className="mt-2 text-base leading-7 text-gray-600" style={{color: '#f8ddc2'}}>{feature.description}</dd>
                 </div>
               ))}
             </dl>
