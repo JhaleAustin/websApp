fc3043632413fb9b6749c083e27c85b63ac0c4cf
RemdelBarela <remdeleiron.barela@tup.edu.ph>
2024-03-03 22:58:27 +0800

    process ui done

--- frontend/src/content/process/process.js
+++ frontend/src/content/process/process.js
@@ -46,7 +46,7 @@ function Process() {
               
                 <div class="row step">
                   <div class="col-md-4 desc">
-                        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                        <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
                           {processes[0] && processes[0].title}
                         </h2>
                         <p className="mt-3">
@@ -61,54 +61,44 @@ function Process() {
                 </div>
 
                 <div class="row step">
-                <div class="col-md-6">
-                    <iframe width="95%" height="350" style={{ marginTop: 50, marginLeft: 37 }} src="https://www.youtube.com/embed/rvy65Z6v77g" frameborder="0" allowfullscreen></iframe>
+                  <div class="col-md-8 desc" id="left-video">
+                    <iframe width="720" height="360" src="https://www.youtube.com/embed/rvy65Z6v77g" frameborder="0" allowfullscreen></iframe>
                   </div>
-                  <div class="col-md-6 d-flex align-items-center justify-content-center">
-                    <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-                      <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-                        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                          {processes[1] && processes[1].title}
-                        </h2>
-                        <p className="mt-6 text-lg leading-8 text-gray-300">
-                          {processes[1] && processes[1].content}
-                        </p>                  </div>
-                      </div>
-                    </div>
+                  <div class="col-md-4 desc" id="ldesc">
+                    <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
+                      {processes[1] && processes[1].title}
+                    </h2>
+                    <p className="mt-3">
+                      {processes[1] && processes[1].content}
+                    </p>               
+                  </div>
                 </div>  
       
                 <div class="row step">
-                <div class="col-md-6 d-flex align-items-center justify-content-center">
-                    <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-                      <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-                        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                          {processes[2] && processes[2].title}
-                        </h2>
-                        <p className="mt-6 text-lg leading-8 text-gray-300">
-                          {processes[2] && processes[2].content}</p>
-                      </div>
-                    </div>
+                  <div class="col-md-4 desc">
+                    <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
+                      {processes[2] && processes[2].title}
+                    </h2>
+                    <p className="mt-3">
+                      {processes[2] && processes[2].content}</p>
                   </div>
-                  <div class="col-md-6">
-                      <iframe width="95%" height="350" style={{ marginTop: 50 }} src="https://www.youtube.com/embed/QuWQ-ihdlU8" frameborder="0" allowfullscreen></iframe>
+                    
+                  <div class="col-md-8 desc" id="right-video">
+                    <iframe width="720" height="360" src="https://www.youtube.com/embed/QuWQ-ihdlU8" frameborder="0" allowfullscreen></iframe>
                   </div>  
                 </div>
 
                 <div class="row step">
-                  <div class="col-md-6">
-                    <iframe width="95%" height="350" style={{ marginTop: 50, marginLeft: 37 }} src="https://www.youtube.com/embed/LuWgFLx4ySk" frameborder="0" allowfullscreen></iframe>
+                  <div class="col-md-8 desc" id="left-video">
+                    <iframe width="720" height="360" src="https://www.youtube.com/embed/LuWgFLx4ySk" frameborder="0" allowfullscreen></iframe>
                   </div>
-                  <div class="col-md-6 d-flex align-items-center justify-content-center">
-                    <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-                      <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-                        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                          {processes[3] && processes[3].title}
-                        </h2>
-                        <p className="mt-6 text-lg leading-8 text-gray-300">
-                          {processes[3] && processes[3].content}
-                        </p>
-                      </div>
-                    </div>
+                  <div class="col-md-4 desc" id="ldesc">
+                    <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
+                      {processes[3] && processes[3].title}
+                    </h2>
+                    <p className="mt-3">
+                      {processes[3] && processes[3].content}
+                    </p>
                   </div>
                 </div>
               </div>
